4f2e72951e96536de2ce6d6d03fc218b34f1939b
Elijah Insua <tmpvar@gmail.com>
2014-07-31 14:49:44 -0700

    convert to tape, add initial failures

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+var test = require('tape');
 var stl = require('../stl');
 var assert = require('assert');
 var fs = require('fs');
@@ -23,102 +24,157 @@ var asciiSTL = fs.readFileSync(__dirname + '/ascii/teapot.stl').toString();
 var array = stl.toObject(asciiSTL);
 var basicarray = stl.toObject(basicAsciiSTL);
 
-var string = stl.fromObject(array);
-compareAscii(string, asciiSTL);
-
 // BINARY
 var binaryArray = stl.toObject(binarySTL);
 var binary = stl.fromObject(binaryArray, true);
 var syncSharkResult = stl.toObject(fs.readFileSync(__dirname + '/binary/shark.stl'));
 var cubeBuffer = fs.readFileSync(__dirname + '/binary/cube-20mm-with-hole.stl');
 
-assert.ok(binarySTL.length === binary.length);
-assert.deepEqual(binary.slice(80), binarySTL.slice(80));
-
-// Convert from binary to ascii back to binary
-var out = stl.fromObject(
-  stl.toObject(
-    stl.fromObject(
-      stl.toObject(
-        binarySTL
-      )
-    )
-  )
-  // binary
-, true);
-
-var cubeResult = { description : null, facets: [] }
-fs.createReadStream(__dirname + '/binary/cube-20mm-with-hole.stl')
-  .pipe(stl.createParseStream())
-  .on('data', function(obj) {
-    if (obj.description) {
-      cubeResult.description = obj.description;
-    } else {
-      cubeResult.facets.push(obj);
-    }
-
-  }).on('end', function() {
-    assert.equal(cubeResult.description.trim(), 'solid cube-20-5mm-wall    facet');
-    assert.equal(cubeResult.facets.length, 32);
-
-    var bufferToObjectToStringToObject =  stl.toObject(
-      stl.fromObject(
-        stl.toObject(cubeBuffer), false
-      )
-    );
-
-    var cubeJSON = JSON.stringify(cubeResult);
-    assert.equal(cubeJSON, JSON.stringify(stl.toObject(cubeBuffer)));
-    assert.equal(cubeJSON, JSON.stringify(bufferToObjectToStringToObject));
-  });
 
 
-var binaryResult = { facets : [] };
-fs.createReadStream(__dirname + '/binary/ship.stl')
-  .pipe(stl.createParseStream())
-  .on('data', function(obj) {
-    if (obj.description) {
-      binaryResult.description = obj.description;
-    } else {
-      binaryResult.facets.push(obj);
-    }
+test('ascii in and out', function(t) {
+  compareAscii(stl.fromObject(array), asciiSTL);
+  t.end();
+});
 
-  }).on('end', function() {
-    assert.deepEqual(binaryResult.verts, binaryArray.verts);
-  });
+test('binary in and out', function(t) {
+  t.ok(binarySTL.length === binary.length);
+  t.deepEqual(binary.slice(80), binarySTL.slice(80));
+  t.end();
+});
+
+test('binary cube with solid in name', function(t) {
+  var cubeResult = { description : null, facets: [] }
+  fs.createReadStream(__dirname + '/binary/cube-20mm-with-hole.stl')
+    .pipe(stl.createParseStream())
+    .on('data', function(obj) {
+      if (obj.description) {
+        cubeResult.description = obj.description;
+      } else {
+        cubeResult.facets.push(obj);
+      }
+
+    }).on('end', function() {
+      t.equal(cubeResult.description.trim(), 'solid cube-20-5mm-wall    facet');
+      t.equal(cubeResult.facets.length, 32);
+
+      var bufferToObjectToStringToObject =  stl.toObject(
+        stl.fromObject(
+          stl.toObject(cubeBuffer), false
+        )
+      );
+
+      var cubeJSON = JSON.stringify(cubeResult);
+      t.equal(cubeJSON, JSON.stringify(stl.toObject(cubeBuffer)));
+      t.equal(cubeJSON, JSON.stringify(bufferToObjectToStringToObject));
+      t.end();
+    });
+});
+
+test('ship.stl', function(t) {
+
+  var binaryResult = { facets : [] };
+  fs.createReadStream(__dirname + '/binary/ship.stl')
+    .pipe(stl.createParseStream())
+    .on('data', function(obj) {
+      if (obj.description) {
+        binaryResult.description = obj.description;
+      } else {
+        binaryResult.facets.push(obj);
+      }
+
+    }).on('end', function() {
+      assert.deepEqual(binaryResult.verts, binaryArray.verts);
+      t.end();
+    });
+});
+
+test('tri.stl', function(t) {
+
+  var asciiResult = { description: null, facets : [] };
+  fs.createReadStream(__dirname + '/ascii/tri.stl')
+    .pipe(stl.createParseStream())
+    .on('data', function(obj) {
+      if (obj.description) {
+        asciiResult.description = obj.description;
+      } else {
+        asciiResult.facets.push(obj);
+      }
+
+    }).on('end', function() {
+      assert.deepEqual(asciiResult, basicarray);
+      t.end();
+    });
+});
+
+test('shark.stl', function(t) {
+  var sharkResult = { description : null, facets: [] }
+  fs.createReadStream(__dirname + '/binary/shark.stl')
+    .pipe(stl.createParseStream())
+    .on('data', function(obj) {
+      if (obj.description) {
+        sharkResult.description = obj.description;
+      } else {
+        sharkResult.facets.push(obj);
+      }
+
+    }).on('end', function() {
+      assert.deepEqual(sharkResult, syncSharkResult);
+      t.end();
+    });
+});
+
+test('effector_base.stl', function(t) {
+  fs.createReadStream(__dirname + '/ascii/effector_base.stl')
+    .pipe(stl.createParseStream())
+    .on('end', function() {
+      t.end();
+    })
+});
+
+test('stl.fromObject without normals (ascii)', function(t) {
+  var o = {
+    description : "binary normals",
+    facets : [
+      {
+        verts : [
+          [1, 1, 0],
+          [0, 0, 0],
+          [1, 0, 0]
+        ]
+      }
+    ]
+  };
+
+  var r = stl.fromObject(o);
+
+  var o2 = stl.toObject(r);
+  console.log(o2);
 
 
-var asciiResult = { description: null, facets : [] };
-fs.createReadStream(__dirname + '/ascii/tri.stl')
-  .pipe(stl.createParseStream())
-  .on('data', function(obj) {
-    if (obj.description) {
-      asciiResult.description = obj.description;
-    } else {
-      asciiResult.facets.push(obj);
-    }
+  t.end();
+});
 
-  }).on('end', function() {
-    assert.deepEqual(asciiResult, basicarray);
-  });
+test('stl.fromObject without normals (binary)', function(t) {
 
+  var o = {
+    description : "binary normals",
+    facets : [
+      {
+        verts : [
+          [1, 1, 0],
+          [0, 0, 0],
+          [1, 0, 0]
+        ]
+      }
+    ]
+  };
 
-var sharkResult = { description : null, facets: [] }
-fs.createReadStream(__dirname + '/binary/shark.stl')
-  .pipe(stl.createParseStream())
-  .on('data', function(obj) {
-    if (obj.description) {
-      sharkResult.description = obj.description;
-    } else {
-      sharkResult.facets.push(obj);
-    }
+  var r = stl.fromObject(o, true);
 
-  }).on('end', function() {
-    assert.deepEqual(sharkResult, syncSharkResult);
-  });
+  var o2 = stl.toObject(r);
 
-fs.createReadStream(__dirname + '/ascii/effector_base.stl')
-  .pipe(stl.createParseStream());
+  t.deepEqual(o2.facets[0].normal, [0, 0, 1]);
 
-
-
+  t.end();
+});
